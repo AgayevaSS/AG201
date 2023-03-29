bb1c1f1ed4e9bfe7b5eee6317ca413966a26c9d7
Shafiga <7c0hgc4@code.edu.az>
2023-03-29 13:23:56 +0400

    first commit

---
+++ input_loops/task1.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 10;
+        double myDouble = 10.5;
+        string myString = "Hello, world!";
+        bool myBool = true;
+        char myChar = 'A';
+
+        Console.WriteLine("My integer is " + myInt);
+        Console.WriteLine("My double is " + myDouble);
+        Console.WriteLine("My string is " + myString);
+        Console.WriteLine("My boolean is " + myBool);
+        Console.WriteLine("My character is " + myChar);
+
+        Console.ReadLine();
+    }
+}
---
+++ input_loops/task2.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 10;
+        float myFloat = Convert.ToSingle(myInt);
+
+        Console.WriteLine("My integer is " + myInt);
+        Console.WriteLine("My float is " + myFloat);
+
+        Console.ReadLine();
+    }
+}
