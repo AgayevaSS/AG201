6ab6e843fe1bfde461121daf3041efbb8bb14512
Shafiga Aghayeva <7c0hgc4@code.edu.az>
2023-03-30 13:46:04 +0400

    Methods5

---
+++ Methods1/Methods5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool logic;
+            bool.TryParse(Console.ReadLine(), out logic);
+            Console.WriteLine(BooleanToAnswer(logic));
+        }
+
+        static string BooleanToAnswer(bool logic)
+        {
+            return logic ? "Beli" : "Xeyir";
+        }
+    }
+}
