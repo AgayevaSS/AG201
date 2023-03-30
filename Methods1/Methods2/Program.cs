f055e15e489d3be7179611fab0bc9cd5a45b3255
Shafiga Aghayeva <7c0hgc4@code.edu.az>
2023-03-30 13:29:49 +0400

    Methods2

---
+++ Methods1/Methods2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 7, 9, 15 };
+            Console.WriteLine(Average(array));
+        }
+
+        static double Average(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return (double)sum / array.Length;
+        }
+    }
+}
